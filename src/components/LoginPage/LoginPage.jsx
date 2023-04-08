d6f4adff72bdd4697632892d5b4fe2a42b1aa838
CJ Simon <cjsimon250@outlook.com>
2023-04-08 16:24:02 -0500

    Combined LoginPage with LoginForm and added some styling

--- src/components/LoginPage/LoginPage.jsx
+++ src/components/LoginPage/LoginPage.jsx
@@ -1,26 +1,96 @@
-import React from 'react';
-import LoginForm from './LoginForm/LoginForm';
-import { useHistory } from 'react-router-dom';
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import { useSelector } from "react-redux";
+import { Typography } from "@mui/material";
+import { Button, TextField, Box } from "@mui/material";
+import { useTheme } from "@emotion/react";
+import { tokens } from "../../theme";
+import { useHistory } from "react-router-dom";
 
 function LoginPage() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const dispatch = useDispatch();
   const history = useHistory();
+  const errors = useSelector((store) => store.errors);
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
+
+  const login = (event) => {
+    event.preventDefault();
+
+    if (email && password) {
+      dispatch({
+        type: "LOGIN",
+        payload: {
+          email: email,
+          password: password,
+        },
+      });
+    } else {
+      dispatch({ type: "LOGIN_INPUT_ERROR" });
+    }
+  }; // end login
 
   return (
-    <div>
-      <LoginForm />
-
-      <center>
-        <button
-          type="button"
-          className="btn btn_asLink"
-          onClick={() => {
-            history.push('/registration');
-          }}
-        >
-          Register
-        </button>
-      </center>
-    </div>
+    <Box
+      pt="10%"
+      width="25vw"
+      margin="auto"
+      sx={{
+        "& .MuiButton-sizeMedium": {
+          backgroundColor: colors.tealAccent[500],
+        },
+        "& .MuiButton-sizeMedium:hover": {
+          backgroundColor: colors.tealAccent[700],
+        },
+      }}
+    >
+      <form className="formPanel" onSubmit={login}>
+        <Typography variant="h2" textAlign="center">
+          Login
+        </Typography>
+        {errors.loginMessage && (
+          <Typography varaint="h3" className="alert" role="alert">
+            {errors.loginMessage}
+          </Typography>
+        )}
+        <Box>
+          <TextField
+            autoFocus
+            margin="dense"
+            fullWidth
+            type="email"
+            label="E-Mail"
+            value={email}
+            onChange={(event) => setEmail(event.target.value)}
+          />
+        </Box>
+        <Box>
+          <TextField
+            autoFocus
+            margin="dense"
+            fullWidth
+            type="password"
+            label="Password"
+            value={password}
+            onChange={(event) => setPassword(event.target.value)}
+          />
+        </Box>
+        <Box display="flex" justifyContent="space-between">
+          <Button type="submit" name="submit" value="Log In">
+            Log In
+          </Button>
+          <Button
+            onClick={() => {
+              history.push("/registration");
+            }}
+          >
+            Register
+          </Button>
+        </Box>
+      </form>
+    </Box>
   );
 }
 
