6f54913e29e21425455043a131c874d0e6fb244d
Cyril Malle-Barlow <cyril.mallebarlow@gmail.com>
2023-04-10 14:07:51 -0500

    router typo

--- server/server.js
+++ server/server.js
@@ -1,21 +1,19 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-require("dotenv").config();
+const express = require('express');
+const bodyParser = require('body-parser');
+require('dotenv').config();
 
 const app = express();
 
-const sessionMiddleware = require("./modules/session-middleware");
-const passport = require("./strategies/user.strategy");
+const sessionMiddleware = require('./modules/session-middleware');
+const passport = require('./strategies/user.strategy');
 
 // Route includes
-const userRouter = require("./routes/user.router");
-const cohortRouter = require("./routes/cohort.router");
-const unitRouter = require("./routes/unit.router");
-<<<<<<< HEAD
-=======
+const userRouter = require('./routes/user.router');
+const cohortRouter = require('./routes/cohort.router');
+const unitRouter = require('./routes/unit.router');
 const lessonRouter = require('./routes/lesson.router');
->>>>>>> a5c9e592a7974a126cb84a955bbd7253c0f32c4e
-const newRegistrantsRouter = require("./routes/newRegistrants.router");
+const newRegistrantsRouter = require('./routes/newRegistrants.router');
+const studentsRouter = require('./routes/students.router');
 
 // Body parser middleware
 app.use(bodyParser.json());
@@ -29,19 +27,15 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 /* Routes */
-app.use("/api/user", userRouter);
-app.use("/api/cohort", cohortRouter);
-app.use("/api/unit", unitRouter);
-<<<<<<< HEAD
-app.use("/api/newRegistrants", newRegistrantsRouter);
-=======
-app.use("/api/lesson", lessonRouter);
-app.use("/api/newRegistrants", newRegistrantsRouter);
-
->>>>>>> a5c9e592a7974a126cb84a955bbd7253c0f32c4e
+app.use('/api/user', userRouter);
+app.use('/api/cohort', cohortRouter);
+app.use('/api/unit', unitRouter);
+app.use('/api/lesson', lessonRouter);
+app.use('/api/newRegistrants', newRegistrantsRouter);
+app.use('/api/students', studentsRouter);
 
 // Serve static files
-app.use(express.static("build"));
+app.use(express.static('build'));
 
 // App Set //
 const PORT = process.env.PORT || 5000;
--- src/redux/sagas/students.saga.js
+++ src/redux/sagas/students.saga.js
@@ -4,16 +4,17 @@ import Swal from 'sweetalert2';
 import withReactContent from 'sweetalert2-react-content';
 
 function* getStudents() {
-    try {
-      let response = yield axios.get("/api/students");
-      yield put({ type: "SET_STUDENTS", payload: response.data });
-    } catch (error) {
-      console.error("Error getting students", error);
-    }
+  try {
+    let response = yield axios.get('/api/students');
+    yield put({ type: 'SET_STUDENTS', payload: response.data });
+    console.log(response.data);
+  } catch (error) {
+    console.error('Error getting students', error);
   }
+}
 
-  function* studentsSaga() {
-    yield takeLatest("GET_STUDENTS", getStudents)
-  }
+function* studentsSaga() {
+  yield takeLatest('GET_STUDENTS', getStudents);
+}
 
-  export default studentsSaga
+export default studentsSaga;
