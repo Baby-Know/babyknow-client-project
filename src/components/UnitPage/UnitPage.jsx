68e7f070f3c98743ce83dee47b56564eedaeb174
Nolen Lawton <mnlawton@icloud.com>
2023-04-10 13:38:48 -0500

    created get for lessons

--- src/components/UnitPage/UnitPage.jsx
+++ src/components/UnitPage/UnitPage.jsx
@@ -1,10 +1,34 @@
 import { useParams } from "react-router-dom"
+import { useEffect } from "react"
+import { useDispatch } from "react-redux";
+import { useSelector } from "react-redux";
 
 function UnitPage () {
+    const dispatch = useDispatch()
     const {id} = useParams()
+    const unitInfo = useSelector(store => store.unit)
+
+    useEffect(() => {
+        dispatch({ 
+            type: "GET_UNIT",
+            payload: id
+         });
+    }, []);
+
 
     return(
-        <h1>UNIT PAGE {id}</h1>
+        <>
+            <h1>{unitInfo[0].unitsName}</h1>
+        
+            {unitInfo.map((unit, i) => {
+                return(
+                    <div key={i}>
+                        <h2>{unit.lessonsName}</h2>
+                        <p>{unit.description}</p>
+                    </div>
+                )
+            })}
+        </>
     )
 }
 
