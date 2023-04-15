6335fef2f2d228055b90d5343afecf1bc3e7c892
CJ Simon <cjsimon250@outlook.com>
2023-04-15 10:42:23 -0500

    Added links and conditional rendering to the registrants page

--- src/components/RegistrantsPage/RegistrantsPage.jsx
+++ src/components/RegistrantsPage/RegistrantsPage.jsx
@@ -1,22 +1,57 @@
 import React from "react";
 import { useSelector } from "react-redux";
+import { useState } from "react";
+import { Box } from "@mui/material";
 import NewRegistrants from "./NewRegistrants/NewRegistrants";
 import Students from "./Students/Students";
 import Teachers from "./Teachers/Teachers";
 
 function RegistrantsPage() {
-  // this component doesn't do much to start, just renders some user reducer info to the DOM
   const user = useSelector((store) => store.user);
+  const [showPage, setShowPage] = useState("newRegistrants");
   return (
-    <div className="container">
-      <h1>Registrants Page!</h1>
-      <h3>New Registrants</h3>
-      <NewRegistrants />
-      <h3>Students</h3>
-      <Students />
-      <h3>Teachers</h3>
-      <Teachers />
-    </div>
+    <Box
+      className="container"
+      sx={{
+        "& .page-links": {
+          cursor: "pointer",
+        },
+      }}
+    >
+      {showPage === "newRegistrants" ? (
+        <h1>New Registrants</h1>
+      ) : showPage === "students" ? (
+        <h1>Students</h1>
+      ) : showPage === "teachers" ? (
+        <h1>Teachers</h1>
+      ) : (
+        <></>
+      )}
+      <Box display="inline-flex" justifyContent={"space-between"} width="20vw">
+        {" "}
+        <h3 onClick={() => setShowPage("teachers")} className="page-links">
+          Teachers
+        </h3>
+        <h3 onClick={() => setShowPage("students")} className="page-links">
+          Students
+        </h3>
+        <h3
+          onClick={() => setShowPage("newRegistrants")}
+          className="page-links"
+        >
+          New Registrants
+        </h3>
+      </Box>
+      {showPage === "newRegistrants" ? (
+        <NewRegistrants />
+      ) : showPage === "students" ? (
+        <Students />
+      ) : showPage === "teachers" ? (
+        <Teachers />
+      ) : (
+        <></>
+      )}
+    </Box>
   );
 }
 
