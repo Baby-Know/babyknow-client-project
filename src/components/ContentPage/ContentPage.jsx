62ed7ea3cf1e699591e9816d51eeb12c08a2654b
Anniessa Antar <anniessa.antar@gmail.com>
2023-04-15 18:12:05 -0500

    updated policy in AWS bucket

--- src/components/ContentPage/ContentPage.jsx
+++ src/components/ContentPage/ContentPage.jsx
@@ -3,13 +3,13 @@ import { useParams } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { Card } from "@mui/material";
 
-function ContentPage () {
+function ContentPage() {
     const { id } = useParams();
     const dispatch = useDispatch();
     const contentArray = useSelector(store => store.contentReducer)
     const content = contentArray[0]
 
-    console.log(content)
+    console.log('content', content)
 
     useEffect(() => {
         dispatch({
@@ -18,16 +18,24 @@ function ContentPage () {
         });
     }, []);
 
-    return(
-        <Card id='contentHeader'>
-                {content ? 
+    return (
+        <>
+            <Card id='contentHeader'>
+                {content ?
                     <>
                         <h1>{content.title}</h1>
-                        <h2>{content.description}</h2> 
+                        <h2>{content.description}</h2>
                     </> :
                     <></>
                 }
-        </Card>
+            </Card>
+
+
+            <video width="320" height="240" controls>
+                <source src="https://baby-know-mn.s3.us-east-2.amazonaws.com/uploads/5b0e441c-51c8-4502-9021-01d69fac8a70-Mindful+Moments+%239.mp4" type="video/mp4" />
+            </video>
+        </>
+
     )
 }
 
