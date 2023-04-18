9e524a9125b2fea5415d94b12193c6daa8442cec
Nolen Lawton <mnlawton@icloud.com>
2023-04-18 12:37:43 -0500

    created content complete visual for required content as well as assigned default cohort upon registration

--- server/routes/students.router.js
+++ server/routes/students.router.js
@@ -147,7 +147,6 @@ router.put('/:id', rejectUnauthenticated, rejectStudent, async (req, res) => {
   const email = req.body.email;
   const cohort = req.body.cohort;
   const studentUnits = req.body.studentUnits;
-  console.log(studentUnits);
 
   try {
     await connection.query('BEGIN');
@@ -212,7 +211,6 @@ router.put('/:id', rejectUnauthenticated, rejectStudent, async (req, res) => {
           selectContentIdsText,
           selectContentIdsParams
         );
-        console.log(result.rows);
         const contentIds = result.rows;
 
         await Promise.all(
