d8ca81146ac51cb357bfc685221ba21c37fb6426
Anniessa Antar <anniessa.antar@gmail.com>
2023-04-19 18:31:04 -0500

    initial setup

--- src/components/AboutPage/AboutPage.jsx
+++ src/components/AboutPage/AboutPage.jsx
@@ -1,20 +1,42 @@
-import React from 'react';
-
-// This is one of our simplest components
-// It doesn't have local state,
-// It doesn't dispatch any redux actions or display any part of redux state
-// or even care what the redux state is'
+import { Box, Grid } from '@mui/material';
 
 function AboutPage() {
   return (
-    <div className="container">
-      <div>
-        <h1>Welcome to Baby Know</h1>
+    <div>
+      <Box 
+      sx={{
+          display: 'flex',
+          justifyContent: "center",
+          alignItems: "center",
+          margin: 'auto',
+          width: '50%',
+          height: '100%'
+      }}
+          >
+        <h1>About Our Program</h1>
+      </Box>
+
+      <Box
+        sx={{
+          display: 'flex',
+          justifyContent: "center",
+          alignItems: "center",
+          margin: "auto",
+          width: '80%',
+          height: '150%',
+          fontSize: "150%"
+        }}
+      >
         <p>
-        Our course is designed to support parents and caregivers throughout their child’s first year of life by providing 
-        easily accessible video lessons covering all aspects of child development in addition to strategies and activities to promote the overall well-being of the entire family. 
-        To learn more about our course, check out the video below. </p>
-      </div>
+          The first twelve months of a baby’s life are a crucial period for learning and brain development. It is often said that “Parents are a child’s first and most important teachers.” Yet, access to information, tools,
+          and strategies for baby’s first year of life is not always readily available or utilized. New parents want the best for their child including all of the tools available,
+          especially when they may not know how to promote or enhance their baby’s development.
+          The BABY KNOW: BODIES, HEARTS & MINDS program provides new parents with a holistic approach to their child’s development.
+          Our program includes information, tools, and strategies to create strong parent-child relationships, incorporating developmental milestones and nurturing the emotional well-being of all family members.
+          Parents will learn through video lectures, demonstrations, and activities, covering all aspects of development.
+        </p>
+
+      </Box>
     </div>
   );
 }
