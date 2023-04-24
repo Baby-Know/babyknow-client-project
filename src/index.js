f845badad90eb786a987e072cce986e7f4817d75
Cyril Malle-Barlow <cyril.mallebarlow@gmail.com>
2023-04-24 13:08:48 -0500

    new mock data in sql

--- src/index.js
+++ src/index.js
@@ -1,18 +1,18 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { Provider } from "react-redux";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
 
-import store from "./redux/store";
+import store from './redux/store';
 
-import App from "./components/App/App";
+import App from './components/App/App';
 
-import "./index.css";
+import './index.css';
 
-const root = ReactDOM.createRoot(document.getElementById("react-root"));
+const root = ReactDOM.createRoot(document.getElementById('react-root'));
 root.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </React.StrictMode>
+  // <React.StrictMode>
+  <Provider store={store}>
+    <App />
+  </Provider>
+  // </React.StrictMode>
 );
